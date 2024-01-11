8c0d8ddb2611a1593700dec838f2040f7caff1a3
amitp138 <amitpande1008@gmail.com>
2024-01-11 18:36:43 +0530

    r

--- src/components/EmailAuth.js
+++ src/components/EmailAuth.js
@@ -1,25 +1,120 @@
-import React from "react";
-import "./UserProfile.css";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import "./EmailAuth.css";
+import { Link, useNavigate } from "react-router-dom";
 
-const UserPageAfterOtpAndEmail = () => {
-  const location = useLocation();
+const EmailAuth = () => {
+  const navigate = useNavigate();
 
-  // Provide default values for email and username or handle the null case
-  const { email = null, username = null } = location.state || {};
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [emailValidation, setEmailValidation] = useState({
+    isValid: false,
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    // Update email validation
+    if (name === "email") {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const isValid = emailRegex.test(value);
+      setEmailValidation({
+        isValid,
+        message:
+          isValid || value === "" ? "" : "Please enter a valid email address.",
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const invalidFields = [];
+
+    if (!emailValidation.isValid) {
+      invalidFields.push("Email");
+    }
+
+    if (invalidFields.length > 0) {
+      alert(`Please fix the following fields: ${invalidFields.join(", ")}`);
+      return;
+    }
+    try {
+      const response = await fetch(`${process.env.REACT_APP_EMAIL_AUTH_URI}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Login successful
+        console.log("Login successful");
+        navigate("/auth/user", {
+          state: {
+            email: formData.email,
+            username: formData.username,
+          },
+        });
+      } else {
+        // Handle Login error
+        console.error("Login failed");
+        alert("Login failed. Please check your credentials and try again.");
+      }
+    } catch (error) {
+      console.error("Error during Login:", error);
+      alert("An unexpected error occurred. Please try again later.");
+    }
+  };
 
   return (
-    <div className="user-profile-container">
-      <div className="user-info">
-        <h1 className="welcome-message">Welcome, to Authflow!</h1>
-        {email != null && <p className="user-email">Email: {email}</p>}
-
-        {username != null && (
-          <p className="user-username">Username: {username}</p>
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="username"
+          className="login-input"
+          placeholder="Username"
+          onChange={handleInputChange}
+          value={formData.username}
+        />
+        <input
+          type="text"
+          name="email"
+          className={`login-input ${
+            emailValidation.isValid ? "valid" : "invalid"
+          }`}
+          placeholder="Email"
+          onChange={handleInputChange}
+          value={formData.email}
+        />
+        {formData.email && emailValidation.message && (
+          <p className="validation-message">{emailValidation.message}</p>
         )}
-      </div>
+        <input
+          type="password"
+          name="password"
+          className="login-input"
+          placeholder="Password"
+          onChange={handleInputChange}
+          value={formData.password}
+        />
+
+        <button type="submit" className="login-button">
+          Sign In
+        </button>
+        <Link to="/forgotpass">Forgot password?</Link>
+      </form>
     </div>
   );
 };
 
-export default UserPageAfterOtpAndEmail;
+export default EmailAuth;
