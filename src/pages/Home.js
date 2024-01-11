4acc30603326360ae01618369ab2355185326946
amitp138 <amitpande1008@gmail.com>
2024-01-11 18:26:47 +0530

    r

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
-    console.log(isAuthenticated);
     if (isAuthenticated) {
       navigate("/auth/user");
+    } else {
+      // Set showContent to true only when authentication status is determined
+      setShowContent(true);
     }
   }, [isAuthenticated, navigate]);
 
   return (
     <>
-      <header>
-        <h1>AuthFlow</h1>
-      </header>
-      <main>
-        <Link to="/signin" className="navigation-link">
-          Sign In
-        </Link>
-        <Link to="/signup" className="navigation-link">
-          Sign Up
-        </Link>
-      </main>
-      <footer></footer>
+      {isLoading ? (
+        // Show loading indicator while checking authentication status
+        <div>Loading...</div>
+      ) : showContent ? (
+        // Show content only when authentication status is determined
+        <>
+          <header>
+            <h1>AuthFlow</h1>
+          </header>
+          <main>
+            <Link to="/signin" className="navigation-link">
+              Sign In
+            </Link>
+            <Link to="/signup" className="navigation-link">
+              Sign Up
+            </Link>
+          </main>
+          <footer></footer>
+        </>
+      ) : null}
     </>
   );
 };
